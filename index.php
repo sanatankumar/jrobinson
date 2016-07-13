e48e3ac1694c2ecdc3d33535b68013092cce1134
Jonathan Robinson <jdswebservice@gmail.com>
2016-07-13 12:41:37 -0400

    Added Work Experience Page
    Added the work experience page. Uploaded new background image for this
    page and edited the work-exp.sass file to reflect the new background
    image.

--- index.php
+++ index.php
@@ -25,23 +25,7 @@
         <div class="main-content">
           <h1>Welcome</h1>
           <p class="h1">Welcome [insert-name], thanks for taking the time to view my website. Here you will find a bunch of information about me, my work history, passsions, and concepts. This website can serve as an online resume if need be. I hope you enjoy your stay.</p>
-          <h2>About Me</h2>
-          <p class="h2">I've worked a lot of jobs since I turned 16. I have worked in the tech industry, medical healthcare industry, music production industry, and food industry. Thats not even half of it! Safe to say I refer to myself as someone who can achieve anything if I put my mind to it.</p>
-          <h3>Medical Healthcare Industry</h3>
-          <p class="h3">In my work as an Emergency Medical Technician, I've learned that not only does it take patience, but discipline as well. My exstensive inner workings with a military style command structure, makes me someone who knows when to follow and when to lead. Being such a "liable" area of work, numerous extensive, detailed, and concise reports had to be made for each and every patient.</p>
-          <p class="h3">At the age of 18 I opened my own business as a "Large Scale Event Medical Supervisor", providing large scale events with basic medical support. This business was not to provide advanced life support to patients, but rather to make a sober, eductated decision on whether EMS had to be brought into the situation. By hiring my company it reduced the need to have an ambulance stationed at the event, saving my clients thousands of dollars, while also keeping the client's patreons safe.</p>
-          <h3>Tech Industry</h3>
-          <p class="h3">I started out in the Tech Industry at the young age of 11 years old. My parents owned a business, and hired a web developer to develop a website for them. This developer grew to a deeper level then just a business relationship, and invited me to learn from him how to develop websites so I could maintain the site for my parents once his work was finished. I obviously accepted, and got right to work.</p>
-          <p class="h3">I started out by learning the basic fundamentals of web development. I learned languages such as HTML and CSS, moved my way toward Javascript, and then fell in love with PHP. Now to further my education I've decided to start learning JADE and SASS. Safe to say these aren't the only programming languages I know.</p>
-          <h3>Music Production Industry</h3>
-          <p class="h3">When I turned 16 I started going out to parties like all normal 16 year old teens do. I started hanging out with the DJ's at the parties, and was taken under their wing and taught all sorts of music production skills. These skills include DJ'ing, Sound Production, Video Production, and more.</p>
-          <p class="h3">When I moved to Pittsburgh, PA in 2014 I started focusing mainly on DJ'ing. The genre's that I mix are Minimal Techno, Minimal, Tech House, Techno, Deep Dub, Trap, Dubstep, and Ambient. These are just the main sub-genre's of EDM that I mix.</p>
-          <p class="h3">In 2016 after my move to Maine, I started recording a releasing a Internet Radio Podcast that features some retro, and modern Minimal, Bassline House, Techno, and Tech House beats. This radio show is growing rapidly with an expeted listener volume at the end of 2016 to reach 500 listens per day. To listen to these tracks please visit my <a href="http://mixcloud.com/djrednight">Mixcloud Page</a>. Alternatively you can visit the <a href="#">Professional DJ</a> page as well for more information.</p>
-          <h3>Food Industry</h3>
-          <p class="h3">My heritage is Italian, so with that being said the statement "I love food" doesn't ever have to be questioned. I've been cooking all my life with some amazing award winning chefs.</p>
-          <p class="h3">I started my first official job at McDonalds, and worked a multiple locations for 4 years, over multiple states. I helped open up a few stores and trained their crew as well. After a relocation I ended my work at McDonalds with the position rank of Shift Leader.</p>
-          <p class="h3">I then furthered my Food Industry career at Eat N' Park where I was the over night chef in charge of an entire commercial kitchen by myself, servicing over 100 patreons a night. Included in my duties was to prep, and clean the kitchen for the next day's shift. While daunting as it might have seemed, it was the funnest food industry job I've held to date.</p>
-        </div>
+          </div>
 
         <!-- Footer of Website -->
         <?php
